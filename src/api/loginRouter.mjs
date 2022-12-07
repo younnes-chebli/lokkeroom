a3f5f1421c82d651c3e5be237646844e39be4777
Younnes Chebli <younnesc@gmail.com>
2022-12-07 15:22:03 +0100

    feat(login): added jwt

--- src/api/loginRouter.mjs
+++ src/api/loginRouter.mjs
@@ -2,17 +2,25 @@ import express from "express";
 import emailExists from "../tools/emailExists.mjs";
 import getUserByEmail from "../database/queries/getUserByEmail.mjs";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+dotenv.config();
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
 const loginRouter = express.Router();
 
 loginRouter.post("/api/login", async(req, res) => {
     try {
         const { email, password } = req.body;
-        const user = await getUserByEmail(email);
+        const row = await getUserByEmail(email);
         if(await emailExists(email)) {
             try {
-                if (await bcrypt.compare(password, user.password)) {
-                    res.status(200).send("Login successful!");
+                if (await bcrypt.compare(password, row.password)) {
+                    const user = {
+                        username: row.username
+                    }
+                    const accessToken = jwt.sign(user, ACCESS_TOKEN);
+                    res.status(200).send({accessToken: accessToken});
                 } else {
                     res.status(401).send("Not Allowed");
                 }
